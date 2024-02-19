1792c78b6256404fa8806f7d3b97668b55da180e
JabbaCloud <126217298+JabbaCloud@users.noreply.github.com>
2024-02-19 11:21:16 -0500

    Combat code added, camera adjusted, ground layer replaced w/ ground sprite.

--- Rapture/Assets/Scripts/PlayerMovement.cs
+++ Rapture/Assets/Scripts/PlayerMovement.cs
@@ -5,14 +5,27 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    // Basic References
     public Rigidbody2D rb;
     public Transform groundCheck;
     public LayerMask groundLayer;
 
+
+    // Movement Reqs
     private float horizontal;
     private float speed = 8f;
     private float jumpingPower = 8f;
     private bool isFacingRight = true;
+
+    // Enemy Damage Reqs
+    public GameObject attackPoint;
+    public float radius;
+    public LayerMask enemies;
+    public int attackDamage = 40;
+
+    // Player Health reqs
+    public int playerHealth = 100;
 
     // Update is called once per frame
     void Update()
@@ -58,5 +71,30 @@ public class PlayerMovement : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         horizontal = context.ReadValue<Vector2>().x;
+    }
+
+    public void Attack(InputAction.CallbackContext context)
+    {
+        // Play Attack Animation
+
+        // Get Keycode performance
+        if(context.performed)
+        {
+
+        // Detects Enemies in range
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.transform.position, radius, enemies);
+
+        // Damages Enemy
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+            Debug.Log("Hit enemy");
+        }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(attackPoint.transform.position, radius);
     }
 }
