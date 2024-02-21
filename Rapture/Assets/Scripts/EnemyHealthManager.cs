3e066c3406aa072d2e3ca49291602f2c9fd01830
JabbaCloud <126217298+JabbaCloud@users.noreply.github.com>
2024-02-21 18:01:44 -0500

    Enemy Attack

--- Rapture/Assets/Scripts/EnemyHealthManager.cs
+++ Rapture/Assets/Scripts/EnemyHealthManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyHealthManager : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
 
     public int maxHealth = 100;
