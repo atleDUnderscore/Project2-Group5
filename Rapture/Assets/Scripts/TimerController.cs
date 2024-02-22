f128ef940b6c9a66757a9279c9b10ece8ec00260
atleDUnderscore <rogers.brandon425@gmail.com>
2024-02-22 16:25:09 -0500

    Timer Script Functional
    Timer script implemented and functional. Needs to be tied to a "Fail" script in some way, probably through setting the player health to 0

---
+++ Rapture/Assets/Scripts/TimerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class TimerController : MonoBehaviour
+{
+    [SerializeField] TMP_Text timerText;
+    float timeRemaining;
+    bool timerRunning;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeRemaining = 180f;
+        timerRunning = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeRemaining > 0 && timerRunning)
+        {
+            timeRemaining -= Time.deltaTime;
+            DisplayTimeRemaining(timeRemaining);
+        }
+        else if(timeRemaining <= 0 && timerRunning)
+        {
+            timerRunning = false;
+            Debug.Log("Player Lost");
+            timerText.text = "00:00";
+        }
+        
+    }
+    
+    public void DisplayTimeRemaining(float timeDisplay)
+    {
+        float timeMin = Mathf.FloorToInt(timeDisplay / 60);
+        float timeSec = Mathf.FloorToInt(timeDisplay % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", timeMin, timeSec);
+    }
+}
