6b99543eecf375a187145ffda50c8885c259a3fc
atleDUnderscore <136766021+atleDUnderscore@users.noreply.github.com>
2024-02-27 18:41:52 -0500

    Enemy Boss has HealthBar in Debug Scene
    Boss has healthbar that works with health and sets properly.

--- Rapture/Assets/Scripts/EnemyHealthManager.cs
+++ Rapture/Assets/Scripts/EnemyHealthManager.cs
@@ -1,23 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealthManager : MonoBehaviour
 {
 
     public int maxHealth = 100;
     int currentHealth;
+    [SerializeField] Slider enemyHBar;
 
     // Start is called before the first frame update
     void Start()
     {
-        currentHealth = maxHealth;    
+        currentHealth = maxHealth;
+        enemyHBar.maxValue = maxHealth;
+        enemyHBar.value = currentHealth;
     }
 
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+        enemyHBar.value = currentHealth;
 
         if (currentHealth <= 0)
         {
