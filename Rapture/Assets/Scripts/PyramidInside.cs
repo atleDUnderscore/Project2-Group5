5acfdb53bbbb8a1211ce9c9ce3cd0393c2b13f36
atleDUnderscore <136766021+atleDUnderscore@users.noreply.github.com>
2024-03-19 18:53:51 -0400

    Level 3 London Made

--- Rapture/Assets/Scripts/PyramidInside.cs
+++ Rapture/Assets/Scripts/PyramidInside.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class PyramidInside : MonoBehaviour
 {
-    [SerializeField] GameObject insideSpawn;
+    [SerializeField] GameObject teleportTo;
+    [SerializeField] int index;
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
-            collision.gameObject.transform.position = insideSpawn.transform.position;
+            collision.gameObject.transform.position = teleportTo.transform.position;
         }
     }
 }
